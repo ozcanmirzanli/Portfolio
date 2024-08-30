148bffd846ae88edc61cfd338370cb1e597c8221
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2024-08-30 22:52:53 +0200

    first structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MainPageComponent } from './main-page/main-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'my-portfolio';
---
+++ src/app/main-page/main-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+  selector: 'app-main-page',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.scss',
+})
+export class MainPageComponent {}
