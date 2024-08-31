92f6211e354caf3beee0330e41c022aa23b3c7dc
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2024-08-31 12:59:18 +0200

    landing page design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainPageComponent],
+  imports: [RouterOutlet, MainPageComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
