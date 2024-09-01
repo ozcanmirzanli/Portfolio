55e5d964f9b9e00f1ba03ab3b0b2b9d03cc0149f
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2024-09-01 22:45:00 +0200

    skills section logos added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
-
+import { MySkillsComponent } from './my-skills/my-skills.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainPageComponent, AboutMeComponent],
+  imports: [
+    RouterOutlet,
+    MainPageComponent,
+    AboutMeComponent,
+    MySkillsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
