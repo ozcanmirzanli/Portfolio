6dc42c38b1bc6b97577302b9f8c180666f59ffbc
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2024-09-02 19:53:55 +0200

    refactoring and added projects section

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-skills.component.html',
-  styleUrl: './my-skills.component.scss'
+  styleUrl: './my-skills.component.scss',
 })
 export class MySkillsComponent {
+  skills = [
+    { name: 'HTML', image: 'assets/img/html.png', alt: 'html' },
+    { name: 'CSS', image: 'assets/img/css.png', alt: 'css' },
+    {
+      name: 'JavaScript',
+      image: 'assets/img/javascript.png',
+      alt: 'javascript',
+    },
+    {
+      name: 'TypeScript',
+      image: 'assets/img/typescript.png',
+      alt: 'typescript',
+    },
+    { name: 'Angular', image: 'assets/img/angular.png', alt: 'angular' },
+    { name: 'Firebase', image: 'assets/img/firebase.png', alt: 'firebase' },
+    { name: 'Git', image: 'assets/img/git.png', alt: 'git' },
+    { name: 'API', image: 'assets/img/api.png', alt: 'api' },
+    { name: 'Scrum', image: 'assets/img/scrum.png', alt: 'scrum' },
+    {
+      name: 'Material Design',
+      image: 'assets/img/material_design.png',
+      alt: 'material design',
+    },
+    {
+      name: 'Continually Learning',
+      image: 'assets/img/learning.png',
+      alt: 'learning',
+    },
+  ];
 
+  // Method to split skills into chunks (rows) of the given size
+  getSkillRows(rowSize: number) {
+    const rows = [];
+    for (let i = 0; i < this.skills.length; i += rowSize) {
+      rows.push(this.skills.slice(i, i + rowSize));
+    }
+    return rows;
+  }
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrl: './portfolio.component.scss',
 })
 export class PortfolioComponent {
-
+  projects = [
+    {
+      title: 'Join',
+      technologies: 'Angular | TypeScript | HTML | CSS',
+      description:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      image: 'assets/img/join.png',
+      imageAlt: 'join',
+      reverseClass: '',
+    },
+    {
+      title: 'El Pollo Loco',
+      technologies: 'JavaScript | HTML | CSS',
+      description:
+        'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      image: 'assets/img/polloloco.png',
+      imageAlt: 'polloloco',
+      reverseClass: 'row-reverse',
+    },
+    {
+      title: 'Pokédex',
+      technologies: 'JavaScript | HTML | CSS | API',
+      description:
+        'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
+      image: 'assets/img/pokedex.png',
+      imageAlt: 'pokedex',
+      reverseClass: '',
+    },
+  ];
 }
