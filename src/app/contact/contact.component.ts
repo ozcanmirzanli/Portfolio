a9b8eb4f7ee187dc37f2738182f68285659d514a
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2024-09-05 12:43:57 +0200

    checkbox & form validation added

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
 
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+    }
+  }
+
+  // Variable to track checkbox state
+  isChecked = false;
+
+  // Toggle function to change the checkbox state
+  checkBoxToggle() {
+    this.isChecked = !this.isChecked;
+  }
+
+  // Get the image source based on the checked state
+  get checkboxImageSrc(): string {
+    return this.isChecked
+      ? 'assets/img/checked.png'
+      : 'assets/img/unchecked.png';
+  }
 }
