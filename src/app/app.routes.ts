50ee9d70963b1fe1dbe9abce3fe4328777a7026e
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2024-09-08 23:52:30 +0200

    active links & refactoring

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ImpressumComponent } from './impressum/impressum.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'my-skills', component: MySkillsComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'impressum', component: ImpressumComponent },
   { path: 'privacy', component: PrivacyPolicyComponent },
 ];
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule, NgStyle } from '@angular/common';
 import { Component, Output, Input, EventEmitter } from '@angular/core';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgStyle, CommonModule],
+  imports: [NgStyle, CommonModule, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
@@ -13,7 +14,19 @@ export class HeaderComponent {
   @Input() isScreenSmall: boolean = false; // Receive screen size state from parent
   @Output() openMenu = new EventEmitter<void>(); // Output event to notify parent to open the burger menu
 
+  activeLink: string = '';
+
+  links = [
+    { href: '#about-me', text: 'About me' },
+    { href: '#my-skills', text: 'Skills' },
+    { href: '#portfolio', text: 'Portfolio' },
+  ];
+
   openBurgerMenu() {
     this.openMenu.emit(); // Emit event to parent component to open the menu
+  }
+
+  setActiveLink(link: string) {
+    this.activeLink = link; // Set the active link when a link is clicked
   }
 }
