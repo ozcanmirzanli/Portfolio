9caadf492598ede34f0f8d507d2116dd545ef31c
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2024-09-09 12:52:56 +0200

    improvements for active links

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,13 @@
 import { CommonModule, NgStyle } from '@angular/common';
-import { Component, Output, Input, EventEmitter } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import {
+  Component,
+  Output,
+  Input,
+  EventEmitter,
+  HostListener,
+} from '@angular/core';
+import { RouterLink, RouterLinkActive, NavigationEnd } from '@angular/router';
+
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -13,6 +20,8 @@ export class HeaderComponent {
   @Input() isScreenSmall: boolean = false; // Receive screen size state from parent
   @Output() openMenu = new EventEmitter<void>(); // Output event to notify parent to open the burger menu
 
+  activeLink: string = '';
+
   links = [
     { href: '#about-me', text: 'About me' },
     { href: '#my-skills', text: 'Skills' },
@@ -21,5 +30,37 @@ export class HeaderComponent {
 
   openBurgerMenu() {
     this.openMenu.emit(); // Emit event to parent component to open the menu
+  }
+
+  setActiveLink(link: string) {
+    // Set active link on click
+    this.activeLink = link;
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.checkActiveLink(); // Check which link should be active on scroll
+  }
+
+  checkActiveLink() {
+    let foundActiveLink = false;
+
+    this.links.forEach((link) => {
+      const section = document.querySelector(link.href);
+
+      if (section) {
+        const rect = section.getBoundingClientRect();
+        // Check if the section is partially within the viewport
+        if (rect.top + 200 < window.innerHeight && rect.bottom > 0) {
+          this.activeLink = link.href;
+          foundActiveLink = true;
+        }
+      }
+    });
+
+    // If no section is found in viewport, reset the active link
+    if (!foundActiveLink) {
+      this.activeLink = '';
+    }
   }
 }
