aa4f020e0203039c08962713271a9a67adde26a3
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2024-09-13 11:23:02 +0200

    burger menu overflow fix

--- src/app/burger-menu/burger-menu.component.ts
+++ src/app/burger-menu/burger-menu.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -18,6 +24,23 @@ export class BurgerMenuComponent {
     { href: '#portfolio', text: 'Portfolio' },
     { href: '#contact', text: 'Contact' },
   ];
+
+  /**
+   * Handles changes to the component's input properties.
+   * Toggles the 'no-scroll' class on the body element
+   * based on the value of 'isMenuOpen'.
+   *
+   * @param {SimpleChanges} changes - An object containing the previous and current values of the component's inputs.
+   */
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['isMenuOpen']) {
+      if (this.isMenuOpen) {
+        document.body.classList.add('no-scroll');
+      } else {
+        document.body.classList.remove('no-scroll');
+      }
+    }
+  }
 
   closeBurgerMenu() {
     this.closeMenu.emit(); // Emit event to parent component to close the menu
